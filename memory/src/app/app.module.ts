8e9ecf88dd95fb4afd24b1dad0ca2eb92ca3ec98
leonbos1 <ktmleon@live.nl>
2022-06-22 16:00:59 +0200

    componenents init

--- memory/src/app/app.module.ts
+++ memory/src/app/app.module.ts
@@ -4,17 +4,20 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    AdminComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
+      { path: 'admin', component: AdminComponent},
       { path: 'login', component: LoginComponent}
     ])
   ],
