939ceee8e02f8e49c9a5ffa2d1486108bebcb5a3
leonbos1 <ktmleon@live.nl>
2022-06-26 12:04:01 +0200

    players deleten

--- memory/src/app/services/player.service.ts
+++ memory/src/app/services/player.service.ts
@@ -8,10 +8,5 @@ export class PlayerService {
 
   constructor(private http: HttpClient) { }
 
-  giveIds(players : any) {
-    for (let i = 1; i < players.length; i++) {
-      players[i].id = i
-    }
-  }
   
 }
